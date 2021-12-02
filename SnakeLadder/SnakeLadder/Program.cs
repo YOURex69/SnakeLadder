609f41eb40dd7312542f7544ae6b0610bfeadae6
Shubhankar kumar singh <shubhankars361@gmail.com>
2021-12-02 14:32:54 +0530

    s-n commit

---
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Turns obj = new Turns();
+            Random ran = new Random();
+            int dai = ran.Next(1, 7);
+
+            int positionPlayer1 = 0, positionPlayer2 = 0, daiNumberOfTime = 0, nextMove2 = 0, nextMove1 = 0;
+
+            while ((positionPlayer1 != 100 && positionPlayer1 < 101) && (positionPlayer2 != 100 && positionPlayer2 < 101))
+            {
+                if (nextMove2 != 6)
+                {
+                    nextMove1 = ran.Next(3);
+                    positionPlayer1 = positionPlayer1 + obj.loopMethod(nextMove1, dai);
+                }
+                if (nextMove1 != 6)
+                {
+                    nextMove2 = ran.Next(3);
+                    positionPlayer2 = positionPlayer2 + obj.loopMethod(nextMove2, dai);
+                }
+
+                daiNumberOfTime++;
+                Console.WriteLine(daiNumberOfTime);
+                Console.WriteLine("player 1 at step :" + positionPlayer1);
+                Console.WriteLine("player 2 at step :" + positionPlayer2);
+            }
+
+            if (positionPlayer1 == 100)
+            {
+                Console.WriteLine("player1 won the game " + positionPlayer1);
+                Console.WriteLine("dai need to played " + daiNumberOfTime + " times");
+            }
+            else
+            {
+                Console.WriteLine("player2 won the game " + positionPlayer2);
+                Console.WriteLine("dai need to played " + daiNumberOfTime + " times");
+            }
+        }
+    }
+}
---
+++ SnakeLadder/SnakeLadder/Turns.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+   public class Turns
+    {
+
+        public int loopMethod(int nextMove1, int dai)
+        {
+            int position = 0;
+            switch (nextMove1)
+            {
+                case 0://no Move
+                    break;
+                case 1://ladder
+                    position = position + dai;
+                    if (position > 100)
+                    {
+                        position = position - dai;
+                    }
+                    break;
+                case 2://snake
+                    position = position - dai;
+                    if (position < 0)
+                    {
+                        position = 0;
+                    }
+                    break;
+            }
+            return position;
+        }
+    }
+}
